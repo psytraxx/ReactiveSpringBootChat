9ff085f5b82696aa11ecbffaead5387262b56c65
Eric Funk <psytraxx@gmail.com>
2024-06-24 10:26:14 +0200

    remove eslint

--- client/src/mockEventSource.ts
+++ client/src/mockEventSource.ts
@@ -2,6 +2,6 @@ Object.defineProperty(window, 'EventSource', {
   writable: true,
   value: jest.fn().mockImplementation(() => ({
     close: jest.fn(() => {}),
-    addEventListener: jest.fn((_event: string, _callback: (_message: MessageEvent) => {}) => {})
+    addEventListener: jest.fn((_event: string, _callback: (event: MessageEvent) => void) => {})
   }))
 });
