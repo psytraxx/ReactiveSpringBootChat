ebb7e159297729716fb24e99f2159d0bb5925b60
Eric Funk <psytraxx@gmail.com>
2025-05-09 09:52:42 +0200

    feat: enhance testing setup with Testcontainers for MongoDB integration

--- backend/src/test/java/com/ssechat/controller/ChatMessageControllerTest.java
+++ backend/src/test/java/com/ssechat/controller/ChatMessageControllerTest.java
@@ -5,17 +5,31 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.reactive.server.WebTestClient;
 import org.springframework.web.reactive.function.BodyInserters;
+import org.testcontainers.containers.MongoDBContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+import org.testcontainers.utility.DockerImageName;
 
 @ExtendWith(SpringExtension.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@Testcontainers
 /*
-  An easy integration test - depends on a running MongoDB which is not ideal and needs further work.
-  Use dedicated Mongo config, setup test collection, etc.
+  Integration test using Testcontainers to spin up a MongoDB instance.
  */
 class ChatMessageControllerTest {
+
+    @Container
+    static MongoDBContainer mongoDBContainer = new MongoDBContainer(DockerImageName.parse("mongo:8")); // Or your desired MongoDB version
+
+    @DynamicPropertySource
+    static void setProperties(DynamicPropertyRegistry registry) {
+        registry.add("spring.data.mongodb.uri", mongoDBContainer::getReplicaSetUrl);
+    }
 
     @Autowired
     private WebTestClient webTestClient;
